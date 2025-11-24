9c789b3dbf48c11aa67a769a2a23f949f0853e79
DezmondSee <165645480+DezmondSee@users.noreply.github.com>
2025-11-25 01:22:42 +0800

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink]  // import router modules
 })
-export class App {
-  protected readonly title = signal('my-resume-angular');
-}
+export class App {}
---
+++ src/app/pages/hobbies/hobbies.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hobbies',
+  templateUrl: './hobbies.html',
+  styleUrls: ['./hobbies.css']
+})
+export class Hobbies {
+
+}
